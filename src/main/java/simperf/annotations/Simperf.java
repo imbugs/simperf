2c7b3080308e7430ab403b02876b95aba68c479f
tinghe <tinghe@ALIPAY-24033>
2012-09-03 18:02:23 +0800

    添加注释

--- src/main/java/simperf/annotations/Simperf.java
+++ src/main/java/simperf/annotations/Simperf.java
@@ -14,10 +14,19 @@ import simperf.config.Constant;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Simperf {
+    /**
+     * 并发线程数年
+     */
     int thread() default 1;
 
+    /**
+     * 每个线程执行次数
+     */
     int count() default 1;
 
+    /**
+     * 统计间隔时间
+     */
     int interval() default 1000;
 
     /**
@@ -25,9 +34,18 @@ public @interface Simperf {
      */
     long maxTps() default -1;
 
+    /**
+     * 是否启用jtl日志，jtl日志可用于jmeter分析
+     */
     boolean jtl() default false;
 
+    /**
+     * 如果启用jtl日志，指定jtl日志的文件
+     */
     String jtlFile() default Constant.DEFAULT_JTL_FILE;
 
+    /**
+     * 指定log日志的文件
+     */
     String logFile() default Constant.DEFAULT_RESULT_LOG;
 }
