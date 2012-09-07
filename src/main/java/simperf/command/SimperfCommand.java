a559c3e2dc8edc92ba2ccda2a8cbc7c3cb4c221b
imbugs <imbugs@126.com>
2012-09-07 12:40:40 +0800

    修改simperfcommand

--- src/main/java/simperf/command/SimperfCommand.java
+++ src/main/java/simperf/command/SimperfCommand.java
@@ -2,8 +2,8 @@ package simperf.command;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
 import simperf.Simperf;
@@ -22,8 +22,13 @@ public class SimperfCommand {
 
     public SimperfCommand(String[] args) {
         this.args = args;
-        options.addOption("t", "thread", true, "[*] number of thread count");
-        options.addOption("c", "count", true, "[*] number of each thread requests count");
+        Option thread = new Option("t", "thread", true, "[*] number of thread count");
+        Option count = new Option("c", "count", true, "[*] number of each thread requests count");
+        thread.setRequired(true);
+        count.setRequired(true);
+
+        options.addOption(thread);
+        options.addOption(count);
         options.addOption("i", "interval", true, "[ ] interval of print messages, default 1000");
         options.addOption("j", true, "[ ] generate jtl report");
         options.addOption("m", "maxtps", true, "[ ] max tps");
@@ -33,46 +38,36 @@ public class SimperfCommand {
     public Simperf create() {
         try {
             cmd = new PosixParser().parse(options, args);
-        } catch (ParseException e1) {
+
+            int thread = Integer.valueOf(cmd.getOptionValue("t"));
+            int count = Integer.valueOf(cmd.getOptionValue("c"));
+            int interval = 1000;
+
+            if (cmd.hasOption("i")) {
+                interval = Integer.valueOf(cmd.getOptionValue("i"));
+            }
+
+            Simperf simperf = new Simperf(thread, count, interval);
+
+            if (cmd.hasOption("j")) {
+                String jtlFile = cmd.getOptionValue("j");
+                JTLResult jtlResult = new JTLResult(jtlFile, simperf.getMonitorThread());
+                SimperfConfig.setConfig(SimperfConfig.JTL_RESULT, jtlResult);
+            }
+
+            if (cmd.hasOption("m")) {
+                int maxTps = Integer.valueOf(cmd.getOptionValue("m"));
+                simperf.setMaxTps(maxTps);
+            }
+            if (cmd.hasOption("l")) {
+                String logFile = cmd.getOptionValue("l");
+                simperf.getMonitorThread().setLogFile(logFile);
+            }
+            return simperf;
+        } catch (Exception e1) {
             new HelpFormatter().printHelp("SimperfCommand options", options);
             return null;
         }
-
-        int thread = 0;
-        if (!cmd.hasOption("t")) {
-            new HelpFormatter().printHelp("SimperfCommand options", options);
-            return null;
-        }
-        thread = Integer.valueOf(cmd.getOptionValue("t"));
-        if (!cmd.hasOption("c")) {
-            new HelpFormatter().printHelp("SimperfCommand options", options);
-            return null;
-        }
-
-        int count = Integer.valueOf(cmd.getOptionValue("c"));
-        int interval = 1000;
-        if (cmd.hasOption("i")) {
-            interval = Integer.valueOf(cmd.getOptionValue("i"));
-        }
-
-        Simperf simperf = new Simperf(thread, count, interval);
-
-        if (cmd.hasOption("j")) {
-            String jtlFile = cmd.getOptionValue("j");
-            JTLResult jtlResult = new JTLResult(jtlFile, simperf.getMonitorThread());
-            SimperfConfig.setConfig(SimperfConfig.JTL_RESULT, jtlResult);
-        }
-
-        if (cmd.hasOption("m")) {
-            int maxTps = Integer.valueOf(cmd.getOptionValue("m"));
-            simperf.setMaxTps(maxTps);
-        }
-        if (cmd.hasOption("l")) {
-            String logFile = cmd.getOptionValue("l");
-            simperf.getMonitorThread().setLogFile(logFile);
-        }
-
-        return simperf;
     }
 
     public CommandLine getCmd() {
