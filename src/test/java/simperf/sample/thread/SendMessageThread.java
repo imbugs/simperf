2f3a278d3520ae84cbbba6b403868a890599e45c
廷和 <tinghe@alipay.com>
2014-08-04 11:45:48 +0800

    replayer test

--- src/test/java/simperf/sample/thread/SendMessageThread.java
+++ src/test/java/simperf/sample/thread/SendMessageThread.java
@@ -1,12 +1,23 @@
 package simperf.sample.thread;
 
+import java.util.concurrent.Semaphore;
+
 import simperf.thread.SimperfThread;
 
 public class SendMessageThread extends SimperfThread {
 
     MessageSender sender;
+    // 流量回放控制
+    Semaphore     replayerSemaphore;
 
     public boolean runTask() {
+        if (null != replayerSemaphore) {
+            try {
+                replayerSemaphore.acquire();
+            } catch (InterruptedException e) {
+                return false;
+            }
+        }
         return sender.send();
     }
 
